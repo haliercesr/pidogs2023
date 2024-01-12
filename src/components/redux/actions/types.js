9b9d23558b75c7cb10adf2c929dd2640141be8fa
HalierC <115671323+haliercesr@users.noreply.github.com>
2024-01-12 12:31:17 -0300

    Delete src directory

--- src/components/redux/actions/types.js
+++
@@ -1,8 +0,0 @@
-export const GET_DOGS="GET_DOGS";
-export const SEARCH_DOGS="SEARCH_DOGS";
-export const QUERY_DOGS="QUERY_DOGS";
-export const FILTER="FILTER";
-export const ORDER="ORDER";
-export const TEMPERAMENTS="TEMPERAMENTS";
-export const FILTERbdd="FILTERbdd";
-
--- src/configURL.js
+++
@@ -1,4 +0,0 @@
-//export const URLfrontend="http://localhost:3000"
-//export const URLSERVER = 'http://localhost:3001'
-export const URLfrontend="https://pidogs2023.vercel.app";
-export const URLSERVER='https://pi-dogs-backend-gamma.vercel.app';
